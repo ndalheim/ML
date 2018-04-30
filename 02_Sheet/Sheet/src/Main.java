140635a596767056eb2cc0a3fadda68893853125
Ken Moller <ken.moller95@gmail.com>
2018-04-30 11:50:51 +0200

    last permute task

--- 02_Sheet/Sheet/src/Main.java
+++ 02_Sheet/Sheet/src/Main.java
@@ -21,6 +21,25 @@ public class Main {
             int numClassAttribute = Integer.parseInt(args[1]);
             Attribute classAttribute = attributes.get(numClassAttribute);
 
+            double accuracy = multipleRun(trainingDataSet,
+                    attributes, classAttribute, 10);
+
+            System.out.println("Average accuracy : " + accuracy);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static double multipleRun(DataSet trainingDataSet,
+                              ArrayList<Attribute> attributes,
+                              Attribute classAttribute,
+                              int iterations) {
+
+        double avgAccuracy = 0.0;
+
+        for (int i = 0; i < iterations; i++) {
+            System.out.println("Round: " +  i);
             System.out.println("Splitting data");
             DataSet testDataSet = trainingDataSet.extractTestDataset(0.33);
             DataSet predictionSet = new DataSet(testDataSet);
@@ -34,12 +53,13 @@ public class Main {
             System.out.println("Making prediction");
             model.predict(predictionSet, classAttribute);
 
-            System.out.println("Accuracy : ");
-            System.out.println(testDataSet.computeAccuracy(predictionSet, classAttribute));
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            double accuracy = testDataSet.computeAccuracy(predictionSet, classAttribute);
+            avgAccuracy += accuracy;
+            System.out.println("Accuracy : " + accuracy);
+            System.out.println();
         }
+
+        return avgAccuracy / (double) iterations;
     }
 
 
